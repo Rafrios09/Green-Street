d5206e8a235ea7f3092b24bbb694aa964be0443d
jobshibu <122998832+jobshibu@users.noreply.github.com>
2024-01-21 22:27:37 +0000

    Added arrows to slider

--- green.js
+++ green.js
@@ -7,18 +7,22 @@ const navCode = `<a class="logo" href="index.html"><img src="/Images/RealLogo.pn
         <li><a href="energy_solutions.html">Energy Solutions</a></li>
         <li><a href="jC.html">Compare</a></li>
         <li><a href="about.html">About us</a></li>
-        <li><a href="#"><a href="#" id="navbtn2">SIGN UP FOR OUR NEWSLETTER!</a></a></li>
+        <li><a href="#newsltr" id="navbtn2">SIGN UP FOR OUR NEWSLETTER!</a></li>
       </ul>
-      <a href="#" id="navbtn">SIGN UP FOR OUR NEWSLETTER!</a>
+      <a href="#newsltr" id="navbtn">SIGN UP FOR OUR NEWSLETTER!</a>
       <span id="hamburger-btn" class="material-symbols-outlined">menu</span>`;
 nav.innerHTML = navCode;
 
 const header = document.querySelector("header");
 const hamburgerBtn = document.querySelector("#hamburger-btn");
 const closeMenuBtn = document.querySelector("#close-menu-btn");
+const newsbtn = document.querySelector("#navbtn2");
 // Toggle mobile menu on hamburger button click
 hamburgerBtn.addEventListener("click", () =>
   header.classList.toggle("show-mobile-menu"),
 );
 // Close mobile menu on close button click
 closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
+
+//Close mobile menu when newsletter button is clicked
+newsbtn.addEventListener("click", () => hamburgerBtn.click());
