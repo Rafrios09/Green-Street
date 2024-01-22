24a9b2b17b082b6f98f6601cf9e4a0f9b33e7138
Rafael Rios <rafrios09@gmail.com>
2024-01-21 19:08:34 -0600

    added newsletter, scss file

---
+++ app.js
@@ -0,0 +1,56 @@
+//Newsletter
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/subscribe", (req, res) => {
+  const { email, js } = req.body;
+  console.log(req.body);
+
+  const mcData = {
+    members: [
+      {
+        email_address: email,
+        status: "pending",
+      },
+    ],
+  };
+
+  const mcDataPost = JSON.stringify(mcData);
+
+  const options = {
+    url: "https://us21.api.mailchimp.com/3.0/lists/a0a071384b",
+    method: "POST",
+    headers: {
+      Authorization: "auth 98c0dd0f8576b0d43c51d4256920999a-us21",
+    },
+    body: mcDataPost,
+  };
+
+  if (email) {
+    request(options, (err, response, body) => {
+      if (err) {
+        res.json({ error: err });
+      } else {
+        if (js) {
+          res.sendStatus(200);
+        } else {
+          res.redirect("/success.html");
+        }
+      }
+    });
+  } else {
+    res.status(404).send({ message: "Failed" });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log("Server started!"));
---
+++ public/green.js
@@ -0,0 +1,28 @@
+//Navigation bar replication
+const nav = document.querySelector(".navbar");
+const navCode = `<a class="logo" href="index.html"><img src="../Images/RealLogo.png" alt="Green Street"></a>
+      <ul class="menu-links">
+        <span id="close-menu-btn" class="material-symbols-outlined">close</span>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="energy_solutions.html">Energy Solutions</a></li>
+        <li><a href="jC.html">Compare</a></li>
+        <li><a href="about.html">About us</a></li>
+        <li><a href="#newsltr" id="navbtn2">SIGN UP FOR OUR NEWSLETTER!</a></li>
+      </ul>
+      <a href="#newsltr" id="navbtn">SIGN UP FOR OUR NEWSLETTER!</a>
+      <span id="hamburger-btn" class="material-symbols-outlined">menu</span>`;
+nav.innerHTML = navCode;
+
+const header = document.querySelector("header");
+const hamburgerBtn = document.querySelector("#hamburger-btn");
+const closeMenuBtn = document.querySelector("#close-menu-btn");
+const newsbtn = document.querySelector("#navbtn2");
+// Toggle mobile menu on hamburger button click
+hamburgerBtn.addEventListener("click", () =>
+  header.classList.toggle("show-mobile-menu")
+);
+// Close mobile menu on close button click
+closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
+
+//Close mobile menu when newsletter button is clicked
+newsbtn.addEventListener("click", () => hamburgerBtn.click());
