e6b5d419ec141811b151f9213e852dd19f4e8895
jobshibu <122998832+jobshibu@users.noreply.github.com>
2024-01-22 03:14:19 +0000

    Changed links in green.js for navbar

--- public/green.js
+++ public/green.js
@@ -3,13 +3,13 @@ const nav = document.querySelector(".navbar");
 const navCode = `<a class="logo" href="index.html"><img src="../Images/RealLogo.png" alt="Green Street"></a>
       <ul class="menu-links">
         <span id="close-menu-btn" class="material-symbols-outlined">close</span>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="energy_solutions.html">Energy Solutions</a></li>
-        <li><a href="jC.html">Compare</a></li>
-        <li><a href="about.html">About us</a></li>
-        <li><a href="#newsltr" id="navbtn2">SIGN UP FOR OUR NEWSLETTER!</a></li>
+        <li><a href="/public/index.html">Home</a></li>
+        <li><a href="/public/energy_solutions.html">Energy Solutions</a></li>
+        <li><a href="/public/jC.html">Compare</a></li>
+        <li><a href="/public/about.html">About us</a></li>
+        <li><a href="/public/index.html#newsltr" id="navbtn2">SIGN UP FOR OUR NEWSLETTER!</a></li>
       </ul>
-      <a href="#newsltr" id="navbtn">SIGN UP FOR OUR NEWSLETTER!</a>
+      <a href="/public/index.html#newsltr" id="navbtn">SIGN UP FOR OUR NEWSLETTER!</a>
       <span id="hamburger-btn" class="material-symbols-outlined">menu</span>`;
 nav.innerHTML = navCode;
 
