63b578dc741909c4b9bea897a6a37f7ea595d30e
Rafael Rios <rafrios09@gmail.com>
2024-01-23 18:42:42 -0600

    fixed home page and energy solutions css paths

--- public/green.js
+++ public/green.js
@@ -1,13 +1,13 @@
 //Navigation bar replication
 const nav = document.querySelector(".navbar");
-const navCode = `<a class="logo" href="../public/index.html"><img src="../public/Images/RealLogo.png" alt="Green Street"></a>
+const navCode = `<a class="logo" href="index.html"><img src="Images/RealLogo.png" alt="Green Street"></a>
       <ul class="menu-links">
         <span id="close-menu-btn" class="material-symbols-outlined">close</span>
-        <li><a href="../public/index.html">Home</a></li>
-        <li><a href="../public/energy_solutions.html">Energy Solutions</a></li>
-        <li><a href="../public/jC.html">Compare</a></li>
-        <li><a href="../public/about.html">About us</a></li>
-        <li><a href="../public/index.html#newsltr" id="navbtn2">SIGN UP FOR OUR NEWSLETTER!</a></li>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="energy_solutions.html">Energy Solutions</a></li>
+        <li><a href="jC.html">Compare</a></li>
+        <li><a href="about.html">About us</a></li>
+        <li><a href="index.html#newsltr" id="navbtn2">SIGN UP FOR OUR NEWSLETTER!</a></li>
       </ul>
       <a href="../public/index.html#newsltr" id="navbtn">SIGN UP FOR OUR NEWSLETTER!</a>
       <span id="hamburger-btn" class="material-symbols-outlined">menu</span>`;
@@ -19,7 +19,7 @@ const closeMenuBtn = document.querySelector("#close-menu-btn");
 const newsbtn = document.querySelector("#navbtn2");
 // Toggle mobile menu on hamburger button click
 hamburgerBtn.addEventListener("click", () =>
-  header.classList.toggle("show-mobile-menu"),
+  header.classList.toggle("show-mobile-menu")
 );
 // Close mobile menu on close button click
 closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
